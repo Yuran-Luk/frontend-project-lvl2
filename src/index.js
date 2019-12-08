ea8f74b89a890efe25b7b990fa3265f7d0c975da
Yuran B <yurii.alekseevich@gmail.com>
2019-12-08 16:44:04 +0300

    add ini-parser

--- src/index.js
+++ src/index.js
@@ -1,6 +1,7 @@
 import path from 'path';
 import fs from 'fs';
 import yaml from 'js-yaml';
+import ini from 'ini';
 import parse from './parsers';
 import render from './utils';
 
@@ -12,6 +13,10 @@ const parsers = [
   {
     action: (pathToFile) => yaml.safeLoad(fs.readFileSync(pathToFile)),
     check: (pathToFile) => path.extname(pathToFile) === '.yml',
+  },
+  {
+    action: (pathToFile) => ini.parse(fs.readFileSync(pathToFile, 'utf-8')),
+    check: (pathToFile) => path.extname(pathToFile) === '.ini',
   },
 ];
 
