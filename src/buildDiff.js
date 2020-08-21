50737157eb42e520a8babd44fda840484f813f27
Yuran B <yurii.alekseevich@gmail.com>
2020-08-21 17:57:54 +0300

    update build

--- src/buildDiff.js
+++ src/buildDiff.js
@@ -2,62 +2,83 @@ import _ from 'lodash';
 
 const getNodeType = (value) => (_.isObject(value) ? 'node' : 'leaf');
 
+const makeNodeActions = [
+  {
+    status: 'unchanged',
+    check: (beforeData, afterData) => _.isEqual(beforeData, afterData),
+    mkNode: (name, cb, beforeData, afterData) => {
+      const type = getNodeType(beforeData);
+      const dataType = type === 'node' ? 'children' : 'value';
+      return {
+        name,
+        type,
+        status: 'unchanged',
+        [dataType]: type === 'node' ? cb(beforeData, afterData) : beforeData,
+      };
+    },
+  },
+  {
+    status: 'add',
+    check: (beforeData, afterData) => !beforeData,
+    mkNode: (name, cb, beforeData, afterData) => {
+      const type = getNodeType(afterData);
+      const dataType = type === 'node' ? 'children' : 'value';
+      return {
+        name,
+        type,
+        status: 'add',
+        [dataType]: type === 'node' ? cb(afterData, afterData) : afterData,
+      };
+    },
+  },
+  {
+    status: 'delete',
+    check: (beforeData, afterData) => !afterData,
+    mkNode: (name, cb, beforeData, afterData) => {
+      const type = getNodeType(beforeData);
+      const dataType = type === 'node' ? 'children' : 'value';
+      return {
+        name,
+        type,
+        status: 'delete',
+        [dataType]: type === 'node' ? cb(beforeData, beforeData) : beforeData,
+      };
+    },
+  },
+  {
+    status: 'updated',
+    check: (beforeData, afterData) => !_.isEqual(beforeData, afterData),
+    mkNode: (name, cb, beforeData, afterData) => {
+      const beforeType = getNodeType(beforeData);
+      const afterType = getNodeType(afterData);
+      const type = afterType === beforeType ? afterType
+        : `${beforeType}/${afterType}`;
+      if (type === 'node') {
+        const children = cb(beforeData, afterData);
+        return {
+          name, type, status: 'updated', children,
+        };
+      }
+      const beforeValue = beforeType === 'node'
+        ? cb(beforeData, beforeData) : beforeData;
+      const afterValue = afterType === 'node'
+        ? cb(afterData, afterData) : afterData;
+      return {
+        name, type, status: 'updated', beforeValue, afterValue,
+      };
+    },
+  },
+];
+
 const diff = (before, after) => {
   const beforeKeys = _.keys(before);
   const afterKeys = _.keys(after);
   const keys = _.union(beforeKeys, afterKeys);
-  const buildAst = keys.map((key) => {
-    const beforeType = getNodeType(before[key]);
-    const afterType = getNodeType(after[key]);
-    const node = {
-      name: key,
-      type: '',
-      status: '',
-    };
-    if (!_.has(after, key)) {
-      node.status = 'delete';
-      node.type = beforeType;
-      if (node.type === 'node') {
-        node.children = diff(before[key], before[key]);
-      } else {
-        node.value = before[key];
-      }
-      return node;
-    }
-    if (!_.has(before, key)) {
-      node.status = 'add';
-      node.type = afterType;
-      if (node.type === 'node') {
-        node.children = diff(after[key], after[key]);
-      } else {
-        node.value = after[key];
-      }
-      return node;
-    }
-    if (_.isObject(before[key]) && _.isObject(after[key])) {
-      node.status = _.isEqual(before[key], after[key]) ? 'unchanged' : 'updated';
-      node.type = 'node';
-      node.children = diff(before[key], after[key]);
-      return node;
-    }
-    if (_.isEqual(before[key], after[key])) {
-      node.status = 'unchanged';
-      node.type = afterType;
-      if (node.type === 'node') {
-        node.children = diff({}, after[key]);
-      } else {
-        node.value = after[key];
-      }
-      return node;
-    }
-    node.status = 'updated';
-    node.type = afterType === beforeType ? afterType
-      : `${beforeType}/${afterType}`;
-    node.beforeValue = getNodeType(before[key]) === 'node'
-      ? diff(before[key], before[key]) : before[key];
-    node.afterValue = getNodeType(after[key]) === 'leaf'
-      ? after[key] : diff(after[key], after[key]);
-    return node;
+  const buildAst = keys.map((name) => {
+    const beforeData = before[name];
+    const afterData = after[name];
+    const { mkNode } = makeNodeActions.find(({ check }) => check(beforeData, afterData));
+    return mkNode(name, diff, beforeData, afterData);
   });
   return buildAst;
 };
