aff29a182cdfc1ffcce92fd06c633c35554e190e
Yuriy B <burulevv@yandex.ru>
2020-12-28 22:14:29 +0300

    step 2

--- bin/gendiff.js
+++ bin/gendiff.js
@@ -1,11 +1,13 @@
 #!/usr/bin/env node
+/* eslint-disable no-console */
 /* eslint-disable import/extensions */
 import commander from 'commander';
 import diff from '../index.js';
 
 commander.version('1.0.0')
   .description('Compares two configuration files and shows a difference.')
-  .arguments('<firstConfig>')
+  .option('-f, --format [type]', 'output format')
+  .arguments('<filepath1> <filepath2>')
   .action((firstConfig) => {
     console.log(diff(firstConfig));
   });
